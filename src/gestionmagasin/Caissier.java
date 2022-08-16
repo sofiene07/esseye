e7c7b2e77e4e356e60ddb062e6f6417108c927a1
sofiene abidi <soufienabidi11@gmail.com>
2022-08-16 01:08:09 +0000

    modification

--- src/gestionmagasin/Caissier.java
+++ src/gestionmagasin/Caissier.java
@@ -2,7 +2,9 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
- */
+ modification
+*/
+
 package gestionmagasin;
 
 /**
